236f5cd05f7a09ddb3b3f7a59b85061f70b5a4fb
SC-201811221724\lanou <642784739@qq.com>
2019-03-25 09:11:23 +0800

    no message

--- first/src/main.js
+++ first/src/main.js
@@ -3,18 +3,13 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-<<<<<<< HEAD
-import axiox from 'axios'
-import Vueaxios from 'vue-axios'
-=======
+
 import axios from 'axios'
 import VueAxios from 'vue-axios'
 import store from './store'//引入store
 
 Vue.use(VueAxios, axios)
->>>>>>> 2bcb7d0116b225dd90966788c46b207855a53c74
 Vue.config.productionTip = false
-Vue.use(Vueaxios,axiox)
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
