ad07f99494274278911daddcfe9dbffe0683dfdd
SC-201811020757\lanou <1056203157@qq.com>
2019-03-25 15:15:47 +0800

    头部

--- first/src/components/header/Header.vue
+++ first/src/components/header/Header.vue
@@ -1,30 +1,30 @@
 <template>
   <section class="container-fluid" >
-    <!--<nav class="nv navbar navbar-fixed-top">-->
-      <!--<router-link class="pull-left" :to="{}">ele.me</router-link>-->
-      <!--<div class="pull-right">-->
-        <!--<span><router-link :to="{name:'login'}">登录</router-link></span>-->
-        <!--<span>|</span>-->
-        <!--<span><router-link :to="{name:'login'}">注册</router-link></span>-->
-      <!--</div>-->
-    <!--</nav>-->
-    <!--<nav class="nv navbar navbar-fixed-top" v-if="isdrag">-->
-      <!--<router-link class="pull-left" :to="{}"> < </router-link>-->
-      <!--<router-link class="pull-center" :to="{}">郑州</router-link>-->
-      <!--<router-link class="pull-right" :to="{}">切换城市</router-link>-->
-    <!--</nav>-->
-    <!--<nav class="nv navbar navbar-fixed-top" v-if="isshow">-->
-    <!--<span class="glyphicon glyphicon-search"></span>-->
-    <!--<router-link class="pull-center" :to="{}">千山区</router-link>-->
-    <!--<div class="pull-right">-->
-    <!--<span><router-link :to="{name:'login'}">登录</router-link></span>-->
-    <!--<span>|</span>-->
-    <!--<span><router-link :to="{name:'login'}">注册</router-link></span>-->
-    <!--</div>-->
-    <!--</nav>-->
-    <nav class="nv navbar navbar-fixed-top" style="height: 1.5rem">
+    <nav class="nv navbar navbar-fixed-top firstHead" v-if="isFirst">
+      <router-link class="pull-left" :to="{}">ele.me</router-link>
+      <div class="pull-right">
+        <span><router-link :to="{name:'login'}">登录</router-link></span>
+        <span>|</span>
+        <span><router-link :to="{name:'login'}">注册</router-link></span>
+      </div>
+    </nav>
+    <nav class="nv navbar navbar-fixed-top firstHead" v-if="isdrag">
+      <router-link class="pull-left" :to="{}"> < </router-link>
+      <router-link :to="{}" class="centerContent">郑州</router-link>
+      <router-link class="pull-right" :to="{}">切换城市</router-link>
+    </nav>
+    <nav class="nv navbar navbar-fixed-top firstHead" v-if="isshow">
+      <span class="glyphicon glyphicon-search"></span>
+      <router-link class="centerContent" :to="{}">千山区</router-link>
+      <div class="pull-right">
+        <span><router-link :to="{name:'login'}">登录</router-link></span>
+        <span>|</span>
+        <span><router-link :to="{name:'login'}">注册</router-link></span>
+      </div>
+    </nav>
+    <nav class="nv navbar navbar-fixed-top bg-warning" :style="{height: '1.5rem',background:'gray',opacity:'0.4'}" v-if="isFour">
       <img src="../../assets/logo.png" alt="" class="pull-left" style="width: 0.8rem; height: 1rem">
-      <div >
+      <div class="introduce">
         <p>效果演示</p>
         <p>
           <span>商家配送</span>
@@ -39,10 +39,8 @@
           <img src="../../assets/logo.png" alt="" style="width: 0.3rem">
           <span>满30减5,满60减8 (APP专享) </span>
         </span>
-        <span class="pull-right">1个活动 > </span>
+        <span class="pull-right bottomContent">1个活动 > </span>
       </div>
-
-
     </nav>
   </section >
 </template>
@@ -51,11 +49,14 @@
 // 头部公共组件
 export default {
   name: "Header",
-  // data(){
-  //   return {
-  //     flag:false
-  //   }
-  // }
+  data(){
+    return {
+      isFirst:true,
+      isdrag:false,
+      isshow:false,
+      isFour:false
+    }
+  }
 }
 </script>
 
@@ -69,11 +70,25 @@ export default {
     color: white;
     width: 100%;
     height: 0.2rem;
-    background-color: blue;
   }
   .leftSmallImg{
     position: absolute;
     left:0;
     bottom:0.05rem;
+  }
+  .firstHead{
+    background-color: blue;
+
+  }
+  .bottomContent{
+    position: absolute;
+    right:0;
+    bottom:0.1rem;
+  }
+  .introduce{
+    margin-left:1rem;
+  }
+  .centerContent{
+   margin-left: 35%;
   }
 </style>
