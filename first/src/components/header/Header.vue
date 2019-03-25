9fd800393e56de7b5d5c5cea603ade1e8d25d8d1
SC-201811020757\lanou <1056203157@qq.com>
2019-03-25 14:03:38 +0800

    图标

--- first/src/components/header/Header.vue
+++ first/src/components/header/Header.vue
@@ -1,13 +1,13 @@
 <template>
   <section class="container-fluid" >
-    <nav class="nv navbar navbar-fixed-top">
-      <router-link class="pull-left" :to="{}">ele.me</router-link>
-      <div class="pull-right">
-        <span><router-link :to="{}">登录</router-link></span>
-        <span>|</span>
-        <span><router-link :to="{}">注册</router-link></span>
-      </div>
-    </nav>
+    <!--<nav class="nv navbar navbar-fixed-top">-->
+      <!--<router-link class="pull-left" :to="{}">ele.me</router-link>-->
+      <!--<div class="pull-right">-->
+        <!--<span><router-link :to="{name:'login'}">登录</router-link></span>-->
+        <!--<span>|</span>-->
+        <!--<span><router-link :to="{name:'login'}">注册</router-link></span>-->
+      <!--</div>-->
+    <!--</nav>-->
     <!--<nav class="nv navbar navbar-fixed-top" v-if="isdrag">-->
       <!--<router-link class="pull-left" :to="{}"> < </router-link>-->
       <!--<router-link class="pull-center" :to="{}">郑州</router-link>-->
@@ -17,12 +17,33 @@
     <!--<span class="glyphicon glyphicon-search"></span>-->
     <!--<router-link class="pull-center" :to="{}">千山区</router-link>-->
     <!--<div class="pull-right">-->
-    <!--<span><router-link :to="{}">登录</router-link></span>-->
+    <!--<span><router-link :to="{name:'login'}">登录</router-link></span>-->
     <!--<span>|</span>-->
-    <!--<span><router-link :to="{}">注册</router-link></span>-->
+    <!--<span><router-link :to="{name:'login'}">注册</router-link></span>-->
     <!--</div>-->
     <!--</nav>-->
+    <nav class="nv navbar navbar-fixed-top" style="height: 1.5rem">
+      <img src="../../assets/logo.png" alt="" class="pull-left" style="width: 0.8rem; height: 1rem">
+      <div >
+        <p>效果演示</p>
+        <p>
+          <span>商家配送</span>
+          <span>/</span>
+          <span>分钟送达</span>
+          <span>/</span>
+          <span>配送费¥5</span>
+        </p>
+        <span class="pull-right" > > </span>
+        <p>公告:欢迎光临,用餐高峰请提前下单,谢谢</p>
+        <span class="pull-left leftSmallImg">
+          <img src="../../assets/logo.png" alt="" style="width: 0.3rem">
+          <span>满30减5,满60减8 (APP专享) </span>
+        </span>
+        <span class="pull-right">1个活动 > </span>
+      </div>
 
+
+    </nav>
   </section >
 </template>
 
@@ -49,5 +70,10 @@ export default {
     width: 100%;
     height: 0.2rem;
     background-color: blue;
+  }
+  .leftSmallImg{
+    position: absolute;
+    left:0;
+    bottom:0.05rem;
   }
 </style>
