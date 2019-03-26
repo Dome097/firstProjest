d750c0b068f38ad2ed73f37a17c0349485db412c
SC-201811221724\lanou <642784739@qq.com>
2019-03-26 09:29:49 +0800

    foot组件添加图标颜色切换,路由切换

--- first/src/components/footer/footGuide.vue
+++ first/src/components/footer/footGuide.vue
@@ -2,20 +2,20 @@
   <div>
     <nav class="navbar navbar-default navbar-fixed-bottom">
       <div class="container-fluid nav_icons">
-        <router-link :to="{}">
-          <i class="iconfont">&#xe722;</i>
+        <router-link :to="{name:'msite'}">
+          <i class="iconfont" :class="{fonc:isChoose1}" @click="changeColor1">&#xe722;</i>
           <p>外卖</p>
         </router-link>
-        <router-link :to="{}">
-          <i class="iconfont">&#xe718;</i>
+        <router-link :to="{name:'search'}" class="router-link-active">
+          <i class="iconfont" :class="{fonc:isChoose2}"  @click="changeColor2">&#xe718;</i>
           <p>搜索</p>
       </router-link>
-        <router-link :to="{}">
-          <i class="iconfont">&#xe75f;</i>
+        <router-link :to="{name:'order'}" class="router-link-active">
+          <i class="iconfont" :class="{fonc:isChoose3}" @click="changeColor3">&#xe75f;</i>
           <p>订单</p>
         </router-link>
-        <router-link :to="{}">
-          <i class="iconfont">&#xe60b;</i>
+        <router-link :to="{name:'profile'}" class="router-link-active">
+          <i class="iconfont" :class="{fonc:isChoose4}" @click="changeColor4">&#xe60b;</i>
           <p>我的</p>
         </router-link>
       </div>
@@ -26,11 +26,51 @@
 <script>
 // 底部公共组件
 export default {
-  name: "footGuide"
+  name: "footGuide",
+  data(){
+    return{
+      isChoose1:false,
+      isChoose2:false,
+      isChoose3:false,
+      isChoose4:false
+    }
+  },
+  methods:{
+    changeColor1(){
+      this.isChoose1 = true;
+      console.log(this.isChoose1)
+
+      this.isChoose2 = false;
+      this.isChoose3 = false;
+      this.isChoose4 = false;
+    },
+    changeColor2(){
+      console.log(222)
+      this.isChoose1 = false;
+      this.isChoose2 = true;
+      this.isChoose3 = false;
+      this.isChoose4 = false;
+    },
+    changeColor3(){
+      this.isChoose1 = false;
+      this.isChoose2 = false;
+      this.isChoose3 = true;
+      this.isChoose4 = false;
+    },
+    changeColor4(){
+      this.isChoose1 = false;
+      this.isChoose2 = false;
+      this.isChoose3 = false;
+      this.isChoose4 = true;
+    }
+  }
 }
 </script>
 
 <style scoped>
+  .fonc{
+    color: #008de1 !important;
+  }
   .nav_icons{
     display: flex;
     justify-content: space-between;
@@ -47,4 +87,5 @@ export default {
   .nav_icons p{
     color: #000;
   }
+
 </style>
