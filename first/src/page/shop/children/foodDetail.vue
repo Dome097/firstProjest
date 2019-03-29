4cdb3a2226ed9d1147170408e36b436cf3cbdc34
SC-201811020757\lanou <1056203157@qq.com>
2019-03-29 19:07:57 +0800

    jym

--- first/src/page/shop/children/foodDetail.vue
+++ first/src/page/shop/children/foodDetail.vue
@@ -25,25 +25,32 @@
     </div>
     <!--食品监督安全公示页面-->
     <div class="foodDetailSafe">
-      <p>
+      <p class="foodDetailSafeF">
         <span class="safePage">食品监督安全公示</span>
-        <i class="iconfont pull-right goSafePage">企业认证详情 &#xe634 </i>
+        <span @click="toShopSafe" class="goSafePage"><i class="iconfont pull-right ">企业认证详情 &#xe634 </i></span>
       </p>
       <div>
-        <img src="" alt="">
+        <div></div>
         <span>监督检查结果:</span>
         <span>差</span>
         <p>检查结果</p>
       </div>
     </div>
+    <!--商家信息部分-->
     <div class="businessInfo">
-      <h3>商家信息</h3>
+      <p>商家信息</p>
       <ul>
         <li>你大爷</li>
         <li>地址:</li>
         <li>营业时间:</li>
-        <li>营业执照</li>
-        <li>餐饮服务许可证</li>
+        <li>
+          <span>营业执照</span>
+          <i class="iconfont pull-right "> &#xe634 </i>
+        </li>
+        <li>
+          <span>餐饮服务许可证</span>
+          <i class="iconfont pull-right "> &#xe634 </i>
+        </li>
       </ul>
     </div>
   </div>
@@ -52,7 +59,12 @@
 <script>
 // 商铺信息页
 export default {
-  name: "foodDetail"
+  name: "foodDetail",
+  methods:{
+    toShopSafe(){
+      this.$router.push({name:'shopSafe'})
+    }
+  }
 }
 </script>
 
@@ -65,13 +77,14 @@ export default {
   /*活动与属性页面*/
   .foodDetailHead{
     background-color: white;
-    height: 1.7rem;
+    height: 2rem;
     width: 100%;
     padding: 0.15rem;
   }
   .foodDetailHead>p{
     border-bottom:0.01rem solid #E3E3E3;
     font-size: 0.24rem;
+    height: 0.4rem;
   }
   .activity>li>span+span{
     color:gray;
@@ -104,11 +117,42 @@ export default {
   .foodDetailSafe{
     background-color: white;
     margin-top: 0.1rem;
-    height: 1rem;
+    height: 1.5rem;
     width: 100%;
     padding: 0.15rem;
   }
+  .foodDetailSafeF{
+    border-bottom:0.01rem solid #E3E3E3;
+    height: 0.4rem;
+  }
+  .safePage{
+    font-size:0.2rem;
+  }
   .goSafePage{
-
+    font-size:0.18rem;
+    color: lightgray;
+    width: 50%;
+    height:0.4rem;
+  }
+  /*商家信息部分*/
+  .businessInfo{
+    background-color: white;
+    margin-top: 0.1rem;
+    height: 3rem;
+    width: 100%;
+    padding: 0.15rem;
+  }
+  .businessInfo>p{
+    border-bottom:0.01rem solid #E3E3E3;
+    font-size: 0.24rem;
+    height: 0.4rem;
+  }
+  .businessInfo>ul>li{
+    margin-bottom: 0.08rem;
+    height: 0.4rem;
+    line-height: 0.4rem;
+  }
+  .businessInfo>ul>li+li{
+    border-top:0.01rem solid #E3E3E3;
   }
 </style>
