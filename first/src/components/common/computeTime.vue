5d82406831c82d96f75298718d986bc368943368
SC-201811011630\lanou <paojiev594@163.com>
2019-04-01 21:43:34 +0800

    过渡动画

--- first/src/components/common/computeTime.vue
+++ first/src/components/common/computeTime.vue
@@ -1,11 +1,44 @@
 <template>
-
+  <div id="#Time">
+    <span>支付剩余时间</span>
+    <span>{{compute}}</span>
+  </div>
 </template>
 
 <script>
+import Vue from 'vue'
+import { MessageBox } from 'mint-ui';
 // 倒计时组件
 export default {
-  name: "computeTime"
+  name: "computeTime",
+  computed:{
+    compute:{
+      get () {
+        return `00:${this.$store.state.dome.minute/10<1?'0'+this.$store.state.dome.minute:this.$store.state.dome.minute}:${this.$store.state.dome.second/10<1?'0'+this.$store.state.dome.second:this.$store.state.dome.second}`
+      },
+      set () {
+        this.$store.commit({type:'getComputeTime'})
+      }
+    }
+  },
+  watch: {
+    compute: {
+      handler () {
+        if (this.$store.state.dome.minute===0&&this.$store.state.dome.second===0)
+        MessageBox.alert('支付超时');
+      },
+      //是否在页面刷新时调用回调函数,默认值是false
+      immediate:true,
+      //深度监听
+      deep:true
+    }
+  },
+  methods: {
+
+  },
+  mounted () {
+      this.$store.commit({type:'getComputeTime'})
+  }
 }
 </script>
 
