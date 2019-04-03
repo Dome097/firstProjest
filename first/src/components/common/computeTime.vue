e2151e08a0d65b875d8b32a63bd2fc876195c505
SC-201811020757\lanou <1056203157@qq.com>
2019-04-03 20:49:38 +0800

    jym

--- first/src/components/common/computeTime.vue
+++ first/src/components/common/computeTime.vue
@@ -1,8 +1,32 @@
 <template>
-  <div id="#Time">
-    <span>支付剩余时间</span>
-    <span>{{compute}}</span>
-  </div>
+  <div class="computerTimeBox">
+    <div id="Time">
+      <p class="paymentTime">支付剩余时间</p>
+      <p class="timeLimit">{{compute}}</p>
+    </div>
+    <div v-if="popupVisible1" :modal = false class="animated heartBeat alertBox1">
+      <div class="remindSet">
+        <i class="iconfont">&#xe632;</i>
+        <p>支付超时</p>
+        <button @click="sureSet">确认</button>
+      </div>
+    </div>
+    <p class="paymentMode">选择支付方式</p>
+    <div class="paymentModeC">
+        <p class="paymentZ">
+          <img src="../../images/zhifubao.png" alt="">
+          <span>支付宝</span>
+          <i class="iconfont pull-right confirm1" @click="pStyle=!pStyle" :class="{style:pStyle}">&#xe604;</i>
+        </p>
+        <p class="paymentW">
+          <img src="../../images/weixin.png" alt="">
+          <span>微信</span>
+          <i class="iconfont pull-right confirm1" @click="pStyle=!pStyle" :class="{style:!pStyle}">&#xe604;</i>
+        </p>
+      </div>
+      <div class="confirmPayment">确认支付</div>
+    </div>
+
 </template>
 
 <script>
@@ -11,6 +35,12 @@ import { MessageBox } from 'mint-ui';
 // 倒计时组件
 export default {
   name: "computeTime",
+  data(){
+    return {
+      pStyle:true,
+      popupVisible1:false
+    }
+  },
   computed:{
     compute:{
       get () {
@@ -25,7 +55,7 @@ export default {
     compute: {
       handler () {
         if (this.$store.state.dome.minute===0&&this.$store.state.dome.second===0)
-        MessageBox.alert('支付超时');
+          this.popupVisible1 = true;
       },
       //是否在页面刷新时调用回调函数,默认值是false
       immediate:true,
@@ -34,7 +64,9 @@ export default {
     }
   },
   methods: {
-
+    sureSet(){
+      this.popupVisible1 = false
+    }
   },
   mounted () {
       this.$store.commit({type:'getComputeTime'})
@@ -43,5 +75,97 @@ export default {
 </script>
 
 <style scoped>
-
+  .computerTimeBox{
+    width: 100%;
+    height: 100%;
+  }
+  #Time{
+    width: 100%;
+    height: 1.5rem;
+    background-color: white;
+    text-align: center;
+  }
+  .paymentTime{
+    padding-top: 0.3rem;
+  }
+  .timeLimit{
+    font-size:0.4rem;
+    font-weight: bolder;
+  }
+  .paymentMode{
+    width: 100%;
+    height: 0.3rem;
+    padding: 0.05rem 0 0.05rem 0.1rem;
+  }
+  .paymentModeC{
+    width: 100%;
+    height: 1.3rem;
+    background-color: white;
+    padding: 0.1rem;
+  }
+  .paymentZ{
+    width: 100%;
+    border-bottom: 0.01rem solid #E3E3E3;
+    padding-bottom: 0.1rem;
+  }
+  .paymentZ>img,.paymentW>img{
+    width: 0.45rem;
+    height: 0.45rem;
+  }
+  .confirmPayment{
+    width: 80%;
+    height: 0.3rem;
+    margin: 0.1rem auto;
+    text-align: center;
+    background-color: #4cd964;
+    line-height: 0.3rem;
+    color: white;
+    font-weight: bolder;
+    border-radius: 0.05rem;
+  }
+  .confirm1{
+    color:white;
+    width: 0.25rem;
+    height: 0.25rem;
+    background-color: gray;
+    padding: 0.04rem;
+    border-radius: 50%;
+  }
+  .style{
+    background-color: #4cd964;
+  }
+  .alertBox1{
+    position: fixed;
+    left: 15%;
+    top: 20%;
+    width: 70%;
+    height: 2rem;
+    border-radius: 0.1rem;
+    background-color: white;
+  }
+  .remindSet{
+    text-align: center;
+    position: relative;
+    height: 2rem;
+  }
+  .remindSet>i{
+    font-size: 0.8rem;
+    color: orange;
+  }
+  .remindSet>p{
+    border: 0;
+  }
+  .remindSet>button{
+    width: 100%;
+    height: 0.4rem;
+    border: 0;
+    margin-bottom: 0;
+    padding: 0;
+    background-color: limegreen;
+    color: white;
+    position: absolute;
+    left: 0;
+    bottom: 0;
+    border-radius:0 0 0.1rem 0.1rem ;
+  }
 </style>
---
+++ first/src/page/vipcard/useCart.vue
@@ -0,0 +1,128 @@
+<template>
+  <div class="exchangeBox">
+    <p class="exchangeHead">成功兑换后将关联到当前账号: <strong>dome</strong></p>
+    <input type="text" placeholder="请输入10位卡号" oninput="if(value.length > 10)value = value.slice(0, 10)" v-model="input1" class="input1">
+    <input type="text" placeholder="请输入6位卡密" oninput="if(value.length > 6)value = value.slice(0, 6)" v-model="input2" class="input2" >
+    <button  @click="exchangeVip" :disabled="input1.length == 10 && input2.length == 6 ? false : true"  :class="{sb:input1.length == 10 && input2.length == 6 ? true : false}">兑换</button>
+    <div class="hint">
+      <p class="hintR">----温馨提示----</p>
+      <div class="hintContent">
+        <p>新兑换的会员服务,权益以「会员说明」为准。</p>
+        <P>月卡: 30次减免配送费。</P>
+        <P>季卡: 90次减免配送费。</P>
+        <P>年卡: 360次减免配送费。</P>
+        <P>*仅限蜂鸟专送订单,每日最多减免3单,每单最高减免4元(一个月按31天计算)</P>
+      </div>
+    </div>
+    <div v-if="!show" :modal = false class="animated heartBeat alertBox1">
+      <div class="remindSet">
+        <i class="iconfont">&#xe632;</i>
+        <p>无效的卡号</p>
+        <button @click="sureSet" class="btn">确认</button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+// 使用卡号购买
+export default {
+  name: "useCart",
+  data(){
+    return {
+      input1:'',
+      input2:'',
+      show:true,
+    }
+  },
+  methods:{
+    exchangeVip(){
+      this.show = false
+    },
+    sureSet(){
+      this.show = true
+    },
+  }
+}
+</script>
+
+<style scoped>
+  .exchangeBox{
+    width: 100%;
+    height: 100%;
+  }
+  .exchangeHead{
+    width: 100%;
+    height: 0.3rem;
+    padding: 0.1rem;
+  }
+  .input1{
+    width: 100%;
+    height: 0.4rem;
+    padding-left: 0.15rem;
+    background-color: white;
+    border-bottom: 0.01rem solid #E3E3E3;
+  }
+  .input2{
+    width: 100%;
+    height: 0.4rem;
+    padding-left: 0.15rem;
+    background-color: white;
+  }
+  button{
+    width: 90%;
+    height: 0.3rem;
+    background-color: gray;
+    border-radius: 0.08rem;
+    color: white;
+    margin: 0.1rem auto;
+    display: table;
+  }
+  .hintR{
+    text-align: center;
+    font-size: 0.16rem;
+  }
+  .hintContent{
+    width: 100%;
+    padding:0.05rem 0.1rem 0 0.4rem ;
+  }
+  .hintContent>p{
+    font-size: 0.12rem;
+  }
+  .sb{
+    background-color: limegreen;
+  }
+  .alertBox1{
+    position: fixed;
+    left: 15%;
+    top: 20%;
+    width: 70%;
+    height: 2rem;
+    border-radius: 0.1rem;
+    background-color: white;
+  }
+  .remindSet{
+    text-align: center;
+    position: relative;
+    height: 2rem;
+  }
+  .remindSet>i{
+    font-size: 0.8rem;
+    color: orange;
+  }
+  .remindSet>p{
+    border: 0;
+  }
+  .remindSet>button{
+    width: 100%;
+    height: 0.4rem;
+    margin-bottom: 0;
+    padding: 0;
+    background-color: limegreen;
+    color: white;
+    position: absolute;
+    left: 0;
+    bottom: 0;
+    border-radius:0 0 0.1rem 0.1rem ;
+  }
+</style>
